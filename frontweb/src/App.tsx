4eb3a14f07c6ef5c5a2a65584d257567b36b2d9c
Kaian Samuel <kaiansamuelster@gmail.com>
2023-06-06 21:40:26 -0300

    Iniciando o Navbar

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,8 +1,12 @@
+import './assets/styles/custom.scss';
 import './App.css';
+import Navbar from 'components/Navbar';
 
 function App() {
-  return (
-    <h1>Hello World!</h1>
+  return(
+  <div>
+    <Navbar />
+  </div>
   );
 }
 
